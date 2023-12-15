94c5d750a108363363de9ec82101f88f8c17826d
zendu <duzhenquan@eastmoney.com>
2023-12-15 16:02:41 +0800

    [2023-12-15 16:02]

--- pipeline.sh
+++ pipeline.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 # Author: zendu 
-# Date: 2021-10-22 11:02:54 
 # Last Modified by:   zendu 
-# Last Modified time: 2022-03-05 15:18:54 
+# Last Modified time: 2023-12-15 15:18:54 
 # 增加了修改MD文件的img路径，上传到服务器的images文件夹
-cd /Users/zendu/Downloads/ymBlog
-rm .*un~ &> /dev/null
 
-gsed -i 's#](img/#](https://ymlog.cn/images/#' source/_posts/*.md
 
-gsed -i 's#src="img/#src="https://ymlog.cn/images/#' source/_posts/*.md
-
-hexo g   
-cp -r source/_posts/img/* public/images/
-rsync -av --delete public root@tencent.cloud:/usr/share/nginx/
-
+# cd /Users/zendu/Downloads/ymBlog
+# rm .*un~ &> /dev/null
+# gsed -i 's#](img/#](https://ymlog.cn/images/#' source/_posts/*.md
+# gsed -i 's#src="img/#src="https://ymlog.cn/images/#' source/_posts/*.md
+# hexo g   
+# cp -r source/_posts/img/* public/images/
+# rsync -av --delete public root@tencent.cloud:/usr/share/nginx/
 # gsed -i 's#](https://ymlog.cn/images/#](img/#' source/_posts/*.md
 
-# 提交代码到gitee
-
 git add .
-
 current_date=$(date +'%Y-%m-%d %H:%M')
-
 if [ "$#" -gt 0 ]; then
     commit_message="[$current_date] $1"
 else
     commit_message="[$current_date]"
 fi
-
 git commit -m "$commit_message"
-
 git push 
